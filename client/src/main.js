f4188a6c520e95ee4ad71858ecd8c90c52ab1cd9
browncow5 <ryan.e.donat@gmail.com>
2021-11-07 16:12:56 +0000

    Add Datadog RUM

--- client/src/main.js
+++ client/src/main.js
@@ -1,3 +1,20 @@
+import { datadogRum } from '@datadog/browser-rum';
+    
+datadogRum.init({
+    applicationId: '376a5b7e-fcf4-4ce5-938a-0469adc03918',
+    clientToken: 'pub2c3a6651a90f063e6fee3e4753dbb8e1',
+    site: 'datadoghq.com',
+    service:'cowlvl-vue',
+    env:'test',
+    // Specify a version number to identify the deployed version of your application in Datadog 
+    // version: '1.0.0',
+    sampleRate: 100,
+    trackInteractions: true,
+    defaultPrivacyLevel: 'allow'
+});
+    
+datadogRum.startSessionReplayRecording();
+
 import { createApp } from 'vue'
 import App from './App.vue'
 
